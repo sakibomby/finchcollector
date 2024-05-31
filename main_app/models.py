1dd88e3c9c91be9e590ccc3a8a5844456705cdc3
sakibomby <csakae@gmail.com>
2024-05-31 12:57:44 -0700

    Part 5

--- main_app/models.py
+++ main_app/models.py
@@ -8,11 +8,22 @@ MEALS = (
     ('D', 'Dinner')
 )
 # Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Finch (models.Model):
     name = models.CharField(max_length=35)
     species = models.CharField(max_length=35)
     description = models.CharField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return f'{self.name} ({self.id})'
